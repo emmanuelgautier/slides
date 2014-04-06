09b156097ed242e0aa8843ce88b9846846486bbc
Emmanuel <manu.gautier1394@gmail.com>
2014-04-06 12:49:52 +0200

    jshint unused variable correction

--- app/controllers/slideshow.js
+++ app/controllers/slideshow.js
@@ -2,34 +2,41 @@
 
 var mongoose = require('mongoose');
 
+/*jshint unused:false */
 exports.index = function(req, res){
 
 };
 
+/*jshint unused:false */
 exports.show = function(req, res){
 
 };
 
 exports.create = {
+	/*jshint unused:false */
 	show: function(req, res){
 
 	},
 
+	/*jshint unused:false */
 	add: function(req, res){
 
 	}
 };
 
 exports.edit = {
+	/*jshint unused:false */
 	show: function(req, res){
 
 	},
 
+	/*jshint unused:false */
 	add: function(req, res){
 
 	}
 };
 
+/*jshint unused:false */
 exports.delete = function(req, res){
 
 };
