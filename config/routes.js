5c40946b7666f9659c5f5539e82d8b807484b85d
Emmanuel <manu.gautier1394@gmail.com>
2014-04-06 12:45:32 +0200

    rectification des routes

--- config/routes.js
+++ config/routes.js
@@ -8,15 +8,15 @@ module.exports = function(app){
 
 	//slideshow management routes
 	var slideshow = require('../app/controllers/slideshow');
-	app.get('slideshow/', slideshow.index);
+	app.get('/slideshow/', slideshow.index);
 
-	app.get('slideshow/:id', slideshow.show);
+	app.get('/slideshow/:id', slideshow.show);
 
-	app.get('slideshow/create', slideshow.create.show);
-	app.post('slideshow/create', slideshow.create.add);
+	app.get('/slideshow/create', slideshow.create.show);
+	app.post('/slideshow/create', slideshow.create.add);
 
-	app.get('slideshow/edit/:id', slideshow.edit.show);
-	app.post('slideshow/edit/:id', slideshow.edit.add);
+	app.get('/slideshow/edit/:id', slideshow.edit.show);
+	app.post('/slideshow/edit/:id', slideshow.edit.add);
 
-	app.get('slideshow/delete/id', slideshow.delete);
+	app.get('/slideshow/delete/id', slideshow.delete);
 };
