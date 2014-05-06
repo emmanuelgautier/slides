470add52fbd7567709dcd1cb97927ea16d6d4073
Emmanuel Gautier <manu.gautier1394@gmail.com>
2014-05-06 17:52:31 +0200

    generation du hash de room
    Reprise du script de génération de hash précédement utilisé pour le
    script slides. Application de cette génération comme valeur par défaut
    pour le token de room dans la base de données.

--- app/controllers/room.js
+++ app/controllers/room.js
@@ -4,15 +4,14 @@ var mongoose = require('mongoose'),
     Room = mongoose.model('Room');
 
 exports.create = function(req, res) {
-  var room = new Room({});
-      room.key = 'toto';
+  var room = new Room();
 
   room.save(function(err, room){
     if (err) {
       res.redirect('/');
       return console.error(err);
     } else {
-      res.redirect('/room/' + room.key);
+      res.redirect('/room/' + room.token);
     }
   });
 };
--- app/models/room.js
+++ app/models/room.js
@@ -1,8 +1,17 @@
 var mongoose = require('mongoose'),
+    crypto = require('crypto'),
     Schema = mongoose.Schema,
 
+    generateToken = function() {
+      //génération d'un hash à partir de la date et d'un nombre aléatoire
+      var hash = crypto.createHash('sha1').update( Date.now().toString() + Math.random().toString() ).digest('base64');
+
+      //suppression des caractères spéciaux
+      return hash.replace(/[^0-9a-zA-Z]+/, '');
+    },
+
     RoomSchema = new Schema({
-      key: { type: String },
+      token: { type: String, default: generateToken() },
       name: { type: String, default: null },
       createdAt: { type: Date, default: Date.now }
     });
