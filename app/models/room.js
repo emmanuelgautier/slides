9aeb02551178a9701cddbd501a32b33ae8c251ff
Emmanuel Gautier <manu.gautier1394@gmail.com>
2014-05-06 21:46:17 +0200

    corrections de bugs
    Correction pour validation grunt
    Correction de la génération de token avec suppression du caractère = de
    fin
    Correction de la génération de token
    Ajout de l'unicité pour le token

--- app/models/room.js
+++ app/models/room.js
@@ -1,17 +1,26 @@
+'use strict';
+
 var mongoose = require('mongoose'),
     crypto = require('crypto'),
     Schema = mongoose.Schema,
 
     generateToken = function() {
       //génération d'un hash à partir de la date et d'un nombre aléatoire
-      var hash = crypto.createHash('sha1').update( Date.now().toString() + Math.random().toString() ).digest('base64');
+      var hash = crypto.createHash('sha1')
+                    .update( Date.now().toString() + Math.random().toString() )
+                    .digest('base64');
 
       //suppression des caractères spéciaux
-      return hash.replace(/[^0-9a-zA-Z]+/, '');
+      hash = hash.replace(/[^0-9a-zA-Z]+/, '');
+
+      //suppression du caractère = de fin
+      hash = hash.substr(0, hash.length - 1);
+
+      return hash;
     },
 
     RoomSchema = new Schema({
-      token: { type: String, default: generateToken() },
+      token: { type: String, index: { unique: true }, default: generateToken },
       name: { type: String, default: null },
       createdAt: { type: Date, default: Date.now }
     });
