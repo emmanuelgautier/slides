99f814b8d84b7b942f47c13bc1a7e8e2158cc713
Emmanuel Gautier <manu.gautier1394@gmail.com>
2014-05-06 15:58:43 +0200

    suppression des erreurs de merge

--- app/controllers/slideshow.js
+++ app/controllers/slideshow.js
@@ -15,21 +15,6 @@ exports.show = function(req, res) {
 
 exports.create = function(req, res) {
 
-  var success = slides.create({
-    name: req.body.name
-  });
-
-<<<<<<< HEAD
-	},
-
-	add: function(req, res) {
-
-	}
-=======
-  if (success) {
-    console.log('created slideshow named', req.body.name);
-  }
->>>>>>> 242d031986291774d2a359a7422cae6e0a622009
 };
 
 exports.edit = {
