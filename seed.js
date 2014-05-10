1e631a23251c4e466323d92f2e52d103984ed2ed
Buzugu <baptiste@baptistegrob.com>
2014-05-10 15:59:18 +0200

    Refactoring du fichier de seed
    Toutes les fonctions de manipulations de base sont désormais dans le même fil rouge, càd que le seed s'effectue dans le callback du clean. Cela permet, à la fin de la chaîne de callbacks, de se déconnecter si le fichier a été appelé directement (condition require.main === module). De même, les opérations effectuées en fonction de l'argument passée en ligne de commande ne s'exécutent que si le fichier est appelé directement (cela évite des effets secondaires si on lance app.js avec l'argument "c"). Plus besoin de process.exit() car une fois déconnecté de la base mongoose il n'y a pas de listeners actif, donc node s'arrête tout seul.

--- seed.js
+++ seed.js
@@ -1,82 +1,65 @@
 'use strict';
 
-    //number process to do
-var actionToDo = 2,
+function seed(mongoose) {
 
-    //number process done
-    actionDone = 0,
+  var seedRooms = [{
+    name: 'Room 1',
+    description: 'This is the first public room',
+    public: true
+  }, {
+    name: 'Room 2',
+    description: 'This is the second private room',
+    public: false
+  }];
 
-    terminateAction = function(){},
+  // Cleaning database when developing
+  mongoose.model('Room').remove({}, function(err) {
 
-    seed = function(mongoose) {
-      // Cleaning database when developing
-      mongoose.model('Room').remove({}, function(err) {
-        if (err) {
-          throw err;
-        }
+    if (err) {
+      throw err;
+    }
+    console.log('Database cleaned.');
 
-        console.log('Database cleaned.');
+    //seeding database with test datas
+    mongoose.model('Room').create(seedRooms, function(err) {
 
-        terminateAction();
-      });
+      if (err) {
+        throw err;
+      }
+      console.log('Database seeded.');
 
-      //seeding database with test datas
-      var seedRooms = [{
-        name: 'Room 1',
-        description: 'This is the first public room',
-        public: true
-      },
-      {
-        name: 'Room 2',
-        description: 'This is the second private room',
-        public: false
-      }];
-
-      mongoose.model('Room').create(seedRooms, function(err) {
-        if(err) {
-          throw err;
-        }
-
-        console.log('Database seeded');
-
-        terminateAction();
-      });
-    };
-
-module.exports = function(mongoose) {
-  seed(mongoose);
-};
-
-//if the process isn't call in express app
-process.argv.forEach(function (val) {
-  //if command line param is passed
-  if(val === 'c') {
-    var config = require('./config/config'),
-        fs = require('fs'),
-        mongoose = require('mongoose');
-
-        //call to terminate process
-        terminateAction = function() {
-          actionDone += 1;
-
-          if(actionDone >= actionToDo) {
-            process.exit(0);
-          }
-        };
-
-    mongoose.connect(config.db);
-    var db = mongoose.connection;
-    db.on('error', function () {
-      throw new Error('unable to connect to database at ' + config.db);
-    });
-
-    var modelsPath = __dirname + '/app/models';
-    fs.readdirSync(modelsPath).forEach(function (file) {
-      if (file.indexOf('.js') >= 0) {
-        require(modelsPath + '/' + file);
+      if (require.main === module) {
+        mongoose.disconnect();
       }
     });
+  });
+}
 
-    seed(mongoose);
-  }
-});
+if (require.main === module) {
+
+  process.argv.forEach(function(val) {
+    //if command line param is passed
+    if (val === 'c') {
+      var config = require('./config/config'),
+          fs = require('fs'),
+          mongoose = require('mongoose');
+
+      mongoose.connect(config.db);
+      var db = mongoose.connection;
+      db.on('error', function() {
+        throw new Error('unable to connect to database at ' + config.db);
+      });
+
+      var modelsPath = __dirname + '/app/models';
+      fs.readdirSync(modelsPath).forEach(function(file) {
+        if (file.indexOf('.js') >= 0) {
+          require(modelsPath + '/' + file);
+        }
+      });
+
+      seed(mongoose);
+    }
+  });
+} else {
+  module.exports = seed;
+}
