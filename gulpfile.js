40c92bb71cb0b81450aaf70aca3233b18e18b43b
Buzugu <baptiste@baptistegrob.com>
2014-06-25 16:53:19 +0200

    Allègement du gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,12 @@
 
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
-var stylish = require('jshint-stylish');
 
 // Lint JavaScript
 gulp.task('jshint', function () {
   return gulp.src(['./**/*.js', '!**/node_modules/**'])
     .pipe(jshint())
-    .pipe(jshint.reporter(stylish))
+    .pipe(jshint.reporter('jshint-stylish'))
     .pipe(jshint.reporter('fail'));
 });
 
