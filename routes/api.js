152c49a7e9002f8e94c8aa9faaa62f7d0cb3f8d6
Emmanuel Gautier <manu.gautier1394@gmail.com>
2014-07-09 18:57:05 +0200

    api router

---
+++ routes/api.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var express = require('express'),
+    router = express.Router();
+
+var room = require('../controllers/room');
+
+//TODO: add api routes
+
+module.exports = {
+  use: '/api',
+  router: router
+};
