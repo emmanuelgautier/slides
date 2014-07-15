6cc8437cdf687a9c9e4a3b935b41a2154cf46e10
Emmanuel Gautier <manu.gautier1394@gmail.com>
2014-07-15 08:12:22 +0200

    fix no new line at end of file

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -4,8 +4,12 @@ slidesApp.controller('AppCtrl', function($scope) {});
 
 slidesApp.controller('HomeCtrl', function($scope, $location) {});
 
-slidesApp.controller('RoomsCtrl', function($scope){});
+slidesApp.controller('RoomsCtrl', function($scope, $http){
+  
+});
 
 slidesApp.controller('RoomCreateCtrl', function($scope){});
 
-slidesApp.controller('RoomShowCtrl', function($scope){});
+slidesApp.controller('RoomShowCtrl', function($scope, $http){
+
+});
