1a2d95b733c81c42e9d5daf3e50a2469e280edc1
Emmanuel Gautier <manu.gautier1394@gmail.com>
2014-08-18 23:20:36 +0200

    add Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,1 +1,50 @@
-'use strict';
+module.exports = function(grunt) {
+  'use strict';
+
+  grunt.util.linefeed = '\n';
+
+  grunt.initConfig({
+
+    jshint: {
+      dev: ['Gruntfile.js', 'app.js', 'config/*.js', 'models/*.js', 'routes/*.js'],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+
+    less: {
+      dev: {
+        options: {
+          compress: true,
+        },
+        files: {
+          'assets/stylesheets/default.css': 'public/css/default.css'
+        }
+      }
+    },
+
+    watch: {
+      stylesheets: {
+        files: ['assets/stylesheets/*.less'],
+        tasks: ['less:dev'],
+        options: {
+          livereload: true
+        }
+      },
+      javascript: {
+        files: ['Gruntfile.js', 'app.js', 'config/*.js', 'models/*.js', 'routes/*.js'],
+        tasks: ['jshint:dev'],
+        options: {
+          livereload: true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['less:dev', 'jshint:dev', 'watch']);
+  grunt.registerTask('prod', ['less']);
+};
