598979b10eab8fa32ba50f0d2aae4fc4f17492f8
Emmanuel Gautier <manu.gautier1394@gmail.com>
2014-08-19 22:39:44 +0200

    clean and seed database in development

--- app/config/db.js
+++ app/config/db.js
@@ -4,33 +4,65 @@ var path = require('path'),
     fs = require('fs'),
     mongoose = require('mongoose'),
 
+    clean = function() {
+      // Cleaning database when developing
+      mongoose.model('Room').remove({}, function(err) {
+
+        if (err) { throw err; }
+        console.log('Database cleaned.');
+      });
+    },
+
     seed = function () {
-  // Seeds the database with bogus info when in development.
+      var randomName = function() {
+        var adjs = [
+            'Autumn', 'Hidden', 'Bitter', 'Misty', 'Silent', 'Empty',
+            'Dry', 'Dark', 'Summer', 'Icy', 'Delicate', 'Quiet', 'White',
+            'Cool', 'Spring', 'Winter', 'Patient', 'Twilight', 'Dawn',
+            'Crimson', 'Wispy', 'Weathered', 'Blue', 'Billowing', 'Broken',
+            'Cold', 'Damp', 'Falling', 'Frosty', 'Green', 'Long', 'Late',
+            'Lingering', 'Bold', 'Little', 'Morning', 'Muddy', 'Old', 'Red',
+            'Rough', 'Still', 'Small', 'Sparkling', 'Throbbing', 'Shy',
+            'Wandering', 'Withered', 'Wild', 'Black', 'Young', 'Holy',
+            'Solitary', 'Fragrant', 'Aged', 'Snowy', 'Proud', 'Floral',
+            'Restless', 'Divine', 'Polished', 'Ancient', 'Purple', 'Lively',
+            'Nameless'
+          ],
 
-  var seedRooms = [{
-    name: 'Room 1',
-    description: 'This is the first public room',
-    public: true
-  }, {
-    name: 'Room 2',
-    description: 'This is the second private room',
-    public: false
-  }];
+          nouns = [
+            'waterfall', 'river', 'breeze', 'moon', 'rain', 'wind', 'sea',
+            'morning', 'snow', 'lake', 'sunset', 'pine', 'shadow', 'leaf',
+            'dawn', 'glitter', 'forest', 'hill', 'cloud', 'meadow', 'sun',
+            'glade', 'bird', 'brook', 'butterfly', 'bush', 'dew', 'dust',
+            'field', 'fire', 'flower', 'firefly', 'feather', 'grass', 'haze',
+            'mountain', 'night', 'pond', 'darkness', 'snowflake', 'silence',
+            'sound', 'sky', 'shape', 'surf', 'thunder', 'violet', 'water',
+            'wildflower', 'wave', 'water', 'resonance', 'sun', 'wood', 'dream',
+            'cherry', 'tree', 'fog', 'frost', 'voice', 'paper', 'frog', 'smoke',
+            'star'
+          ];
 
-  // Cleaning database when developing
-  mongoose.model('Room').remove({}, function(err) {
+        return adjs[Math.floor(Math.random()*(adjs.length-1))] + ' ' +
+          nouns[Math.floor(Math.random()*(nouns.length-1))];
+      };
 
-    if (err) { throw err; }
-    console.log('Database cleaned.');
+      var seedRooms = [{
+        name: randomName(),
+        description: 'This is the first public room',
+        public: true
+      }, {
+        name: randomName(),
+        description: 'This is the second private room',
+        public: false
+      }];
 
-    //seeding database with test datas
-    mongoose.model('Room').create(seedRooms, function(err) {
+      //seeding database with test datas
+      mongoose.model('Room').create(seedRooms, function(err) {
 
-      if (err) { throw err; }
-      console.log('Database seeded.');
-    });
-  });
-};
+        if (err) { throw err; }
+        console.log('Database seeded.');
+      });
+    };
 
 module.exports = function(config){
   var modelsPath = config.root + '/app/models', db;
@@ -48,7 +80,8 @@ module.exports = function(config){
     }
   });
 
-  if (process.env.NODE_ENV === 'development') {
+  if (config.env === 'development') {
+    clean();
     seed();
   }
 };
