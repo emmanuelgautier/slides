c3ef976d70e5b9e6e3702aa18feda8c6790447a8
Emmanuel Gautier <manu.gautier1394@gmail.com>
2014-08-21 22:19:49 +0200

    angular list rooms controller

--- app/assets/js/controllers.js
+++ app/assets/js/controllers.js
@@ -5,7 +5,9 @@ slidesApp.controller('AppCtrl', function($scope) {});
 slidesApp.controller('HomeCtrl', function($scope, $location) {});
 
 slidesApp.controller('RoomsCtrl', function($scope, $http){
-  
+  $http.get('api/room/').success(function(data) {
+  	$scope.rooms = data;
+  });
 });
 
 slidesApp.controller('RoomCreateCtrl', function($scope){});
