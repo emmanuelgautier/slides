930138e0a4e1955dc01780c5bed64bd85cd6662d
Emmanuel Gautier <manu.gautier1394@gmail.com>
2014-09-07 18:28:00 +0200

    fix bug - socket.io no listen sockets

--- app.js
+++ app.js
@@ -3,13 +3,13 @@
 var express = require('express'),
     config = require('./app/config/config'),
 
-    app = express(),
-    server = require('http').Server(app);
+    app = express();
 
 require('./app/config/db')(config);
 require('./app/config/express')(app, config);
-require('./app/config/io')(server, config);
 
 var server = app.listen(app.get('port'), function() {
   console.log('Express server listening on port', server.address().port);
 });
+
+require('./app/config/io')(server, config);
