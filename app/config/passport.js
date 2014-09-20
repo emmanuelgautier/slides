c53bc791a83e3d5185805ed8cf9bf0b525256a68
Emmanuel Gautier <manu.gautier1394@gmail.com>
2014-09-20 16:07:11 +0200

    passport local

--- app/config/passport.js
+++ app/config/passport.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
     GoogleStrategy = require('passport-google-oauth').OAuth2Strategy,
     GitHubStrategy = require('passport-github').Strategy,
     mongoose = require('mongoose'),
@@ -9,6 +10,21 @@ var passport = require('passport'),
 module.exports = function(app, config) {
   app.use(passport.initialize());
   app.use(passport.session());
+
+  passport.use(new LocalStrategy(
+    function(username, password, done) {
+      User.findOne({ username: username }, function(err, user) {
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      });
+    }
+  ));
 
   passport.use(new GoogleStrategy(config.passport.google, 
     function(accessToken, refreshToken, profile, done) {
