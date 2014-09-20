b91b714788cb34342a49a643a22242ce7fb60fc0
Emmanuel Gautier <manu.gautier1394@gmail.com>
2014-09-20 23:16:16 +0200

    small refactoring

--- app/config/express.js
+++ app/config/express.js
@@ -8,18 +8,21 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     hbs = require('express-hbs');
 
-module.exports = function(app, config){
+module.exports = function(app, config) {
+  app.use(favicon( config.root + '/public/favicon.ico'));
+  app.use(logger('dev'));
+
+  app.use(express.static(config.root + '/public'));
+
+  app.use(cookieParser());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
   app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
-  app.use(cookieParser());
   app.use(session({
     secret: 'key',
     saveUninitialized: true,
     resave: true
   }));
-
-  require('../routes/')(app);
 
   app.set('view engine', 'hbs');
   app.set('views', config.root + '/app/views');
@@ -30,50 +33,4 @@ module.exports = function(app, config){
     defaultLayout: config.root + '/app/views/layouts/main.hbs',
     extname: '.hbs',
   }));
-
-  app.use(favicon( config.root + '/public/favicon.ico'));
-  app.use(logger('dev'));
-
-  app.use(express.static(config.root + '/public'));
-
-  /// catch 404 and forward to error handler
-  app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-
-    next(err);
-  });
-
-  /// error handlers
-
-  // development error handler
-  // will print stacktrace
-  // (requires all 4 arguments to be considered an error handler)
-  if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-      /*jshint unused:false*/
-      res.status(err.status || 500);
-      res.render('error', {
-        message: err.message,
-        error: err
-      });
-
-      console.error(err.message);
-    });
-  }
-
-  // production error handler
-  // no stacktraces leaked to user
-  app.use(function(err, req, res, next) {
-    /*jshint unused:false*/
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: {}
-    });
-
-    console.error(err.message);
-  });
-
-  app.set('port', process.env.PORT || config.port);
 };
