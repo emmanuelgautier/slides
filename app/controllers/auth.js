4c1eb722beb52c5d3fb82c851868f75c2168bda6
Emmanuel Gautier <manu.gautier1394@gmail.com>
2014-11-05 23:06:00 +0100

    remove whitespace

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -7,7 +7,7 @@ exports.authenticate = {
   local: function(username, password, done) {
     User.findOne({ username: username }, function(err, user) {
       if (err) { return done(err); }
-      
+
       if (!user) {
         return done(null, false, { message: 'Incorrect username.' });
       }
