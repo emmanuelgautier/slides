e1217dce2ecb33a27102736606764096f5b6ac9a
Emmanuel Gautier <manu.gautier1394@gmail.com>
2014-11-26 22:59:53 +0100

    reindent angular app js file

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -2,22 +2,23 @@ define(['require', 'angular', 'app/config', 'app/run',
   'app/services/auth', 'app/services/rooms', 'app/services/session', 'app/services/socket',
   'app/controllers/home', 'app/controllers/rooms'],
   function(require, angular, config, run, $auth, Room, $session, $socket, HomeController, RoomsController) {
-  'use strict';
+    'use strict';
 
-  var app = angular.module('slides', ['ngRoute', 'ngResource']);
+    var app = angular.module('slides', ['ngRoute', 'ngResource']);
 
-  app.config(config);
+    app.config(config);
 
-  //load factories
-  app.factory('$auth', $auth)
-     .factory('$session', $session)
-     .factory('$socket', $socket)
-     .factory('Room', Room);
+    //load factories
+    app.factory('$auth', $auth)
+       .factory('$session', $session)
+       .factory('$socket', $socket)
+       .factory('Room', Room);
 
-  app.controller('HomeController', HomeController)
-     .controller('RoomsController', RoomsController);
+    app.controller('HomeController', HomeController)
+       .controller('RoomsController', RoomsController);
 
-  app.run(run);
+    app.run(run);
 
-  return app;
-});
+    return app;
+  }
+);
