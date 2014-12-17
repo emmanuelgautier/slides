f3fca180aba62d792670484141ecebc7fec8bc0f
Emmanuel Gautier <manu.gautier1394@gmail.com>
2014-12-17 23:27:21 +0100

    api users

--- app/controllers/api/user.js
+++ app/controllers/api/user.js
@@ -1,21 +1,32 @@
 'use strict';
 
+var mongoose = require('mongoose'),
+    User     = mongoose.model('User');
+
 exports.list = function(req, res, next) {
-  //
+  User.find({ public: true }, function(err, users) {
+    if(err) { next(err); }
+
+    res.json(users);
+  });
 };
 
 exports.show = function(req, res, next) {
-  //
+  User.findOne({ username: req.params.username }, function(err, user) {
+    if(err) { next(err); }
+
+    if(room) {
+      res.json(user);
+    } else {
+      next();
+    }
+  });
 };
 
-exports.create = function(req, res, next) {
-  //
+exports.me = function(req, res) {
+  res.json(req.user);
 };
 
 exports.update = function(req, res, next) {
-  //
-};
-
-exports.delete = function(req, res, next) {
   //
 };
--- app/routes/api.js
+++ app/routes/api.js
@@ -3,7 +3,8 @@
 var express = require('express'),
     router  = express.Router(),
 
-    room    = require('../controllers/api/room');
+    room    = require('../controllers/api/room'),
+    user    = require('../controllers/api/user');
 
 router
   .get('/rooms/',          room.list)
@@ -11,6 +12,12 @@ router
   .post('/rooms/',         room.create)
   .put('/rooms/:token',    room.update)
   .delete('/rooms/:token', room.delete);
+
+router
+  .get('/users/',             user.list)
+  .get('/users/me',           user.me)
+  .get('/users/:username',    user.show)
+  .put('/users/:username',    user.update);
 
 module.exports = {
   use: '/api',
