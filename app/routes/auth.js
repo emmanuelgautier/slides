7dbb2c2e628ef4638be1912149d99d596a32ddea
Emmanuel Gautier <manu.gautier1394@gmail.com>
2014-12-21 22:44:56 +0100

    redirect if user is connected

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -9,10 +9,14 @@ var express  = require('express'),
 
 router
   .get('/register', function(req, res) {
+    if(req.user) { return res.redirect(301, '/'); }
+
     res.render('auth/register', { noangular: true });
   })
   .post('/register', auth.register)
   .get('/login', function(req, res) {
+    if(req.user) { return res.redirect(301, '/'); }
+
     res.render('auth/login', { noangular: true, error: req.flash('error') });
   })
   .post('/login', passport.authenticate('local', {
