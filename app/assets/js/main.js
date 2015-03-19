beb1939e0b12d22ba463f75c0b46944b30c23db8
Emmanuel Gautier <manu.gautier1394@gmail.com>
2015-03-19 22:12:11 +0100

    avoid js async loading

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,31 +1,5 @@
 'use strict';
 
-require.config({
-  baseUrl: "/js",
-  maps: {},
-  paths: {
-    "angular": "lib/angular",
-    "foundation": "lib/foundation",
-
-    "socketio": "lib/socket.io",
-
-    "slideshow": "app/services/slideshow",
-    "slideshare": "app/services/slideshare"
-  },
-  shim: {
-    "angular": {
-      exports: "angular"
-    },
-    "foundation": {
-        exports: "foundation",
-        deps: ['angular']
-    },
-    "socketio": {
-      exports: "io"
-    }
-  }
-});
-
 require(['angular', 'foundation', 'app/app'], 
   function(angular, foundation, app) {
     'use strict';
